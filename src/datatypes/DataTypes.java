b51c2a2fe4a589ccf870ac449e73490900aa4043
jagad <jagad@Priyanka>
2026-02-27 00:56:07 -0500

    Datatypes commit

---
+++ src/datatypes/DataTypes.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class DataTypes {
+	int num=23;
+	static int number;//no value assigned output is 0
+	static double num2;//no value assigned output is 0.0
+	static boolean isSunday;
+	static char ch=89;
+	public static void main(String[] args)
+	{
+		System.out.println(number);
+		System.out.println(num2);
+		System.out.println(isSunday);
+		System.out.println(ch);
+	}
+
+}
