f4005e38b04b691240406f8ee5bbb4dcc8c7b817
jagad <jagad@Priyanka>
2026-02-27 00:56:37 -0500

    Calculator program commit

--- src/calci/Calculator.java
+++ src/calci/Calculator.java
@@ -1,23 +1,20 @@
 package calci;
 
 public class Calculator {
-	int num1 = 25;
-	int num2 = 12;
-
-	public void add() {
-		System.out.println(num1 + num2);
+	public void add(int num1, int num2) {
+		System.out.println("Addition is" + (num1 + num2));
 	}
 
-	public void substract() {
+	public void substract(int num1, int num2) {
 		System.out.println(num1 - num2);
 	}
 
-	public void multiply() {
-		System.out.println(num1 * num2);
+	public void multiply(int num1, int num2, String msg) {
+		System.out.println(msg + ( num1 * num2));
 	}
 
-	public void divide() {
-		System.out.println(num1 / num2);
+	public void divide(int num1, int num2,String msg) {
+		System.out.println(msg + (num1 / num2));
 	}
 
 }
--- src/calci/TestCalculator.java
+++ src/calci/TestCalculator.java
@@ -3,9 +3,9 @@ package calci;
 public class TestCalculator {
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.add();
-		calculator.substract();
-		calculator.multiply();
-		calculator.divide();
+		calculator.add(20, 22);
+		calculator.substract(12, 6);
+		calculator.multiply(12,12,"Multiplication is  ");
+		calculator.divide(40, 4,"Division value is  ");
 	}
 }
