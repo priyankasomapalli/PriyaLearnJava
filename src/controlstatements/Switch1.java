c50677aa12be06cb1c9b5d0ca34ccab31ab9283d
jagad <jagad@Priyanka>
2026-03-01 23:01:22 -0500

    Switch in java

--- src/controlstatements/Switch1.java
+++ src/controlstatements/Switch1.java
@@ -1,7 +1,27 @@
 package controlstatements;
 
 public class Switch1 {
-	//Math marks
-	//
+	// Loan Approval
+	// Based on credit score
+	public void checkingCreditScore(int score) {
+		switch (score) {
+		case 200:
+			System.out.println("Cant approve loan");
+			break;
+		case 400:
+			System.out.println("Loan can be approved");
+			break;
+		case 600:
+			System.out.println("Loan can be approved");
+			break;
+		default:
+			System.out.println("Insufficient credit score");
+		}
 
+	}
+
+	public static void main(String[] args) {
+		Switch1 sw = new Switch1();
+		sw.checkingCreditScore(200);
+	}
 }
