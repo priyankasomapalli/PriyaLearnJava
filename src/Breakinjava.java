69ea40b42380e1bd712cf6bfa6651e56b6e5c06f
jagad <jagad@Priyanka>
2026-03-04 15:03:50 -0500

    Break and continue statements in java

---
+++ src/Breakinjava.java
@@ -0,0 +1,17 @@
+
+public class Breakinjava {
+public static void main(String[] args)
+{
+	for(int i=0;i<=10;i++)
+	{
+		System.out.println("Value of i: "+i);
+		if(i==4)
+		{
+			System.out.println("i value has reached to 4");
+			break;
+		}
+		System.out.println("loop will break at i value is 4");
+		
+	}
+}
+}
---
+++ src/DoWhileinjava.java
@@ -0,0 +1,21 @@
+
+public class DoWhileinjava {
+public void printingNumbers(int a)
+{
+	a=1;
+	do
+	{
+		
+		System.out.println(a);
+		a++;
+		
+		
+	}while(a<15);
+}
+public static void main(String[] args)
+{
+	DoWhileinjava dwj=new DoWhileinjava();
+	dwj.printingNumbers(15);
+	
+}
+}
