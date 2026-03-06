28a247bff31d524e714602d8626f393276b2a246
jagad <jagad@Priyanka>
2026-03-06 17:02:55 -0500

    Counting digits

---
+++ src/CountDigits.java
@@ -0,0 +1,21 @@
+
+public class CountDigits {
+	int count=0;
+	public int countingDigits(int num)
+	{
+	while(num>0) {
+		num=num/10;
+		count++;
+	}
+	return count;
+		
+	}
+	public static void main(String[] args)
+	{
+		CountDigits cd=new CountDigits();
+		int total=cd.countingDigits(489);
+		System.out.println(total);
+		
+	}
+
+}
